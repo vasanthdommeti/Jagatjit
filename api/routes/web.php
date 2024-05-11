224d1022d40a14e70739997b1a0a696b3ff624a3
Pratik Suresh Jadhav <psbj41@gmail.com>
2024-05-12 00:10:12 +0530

    Bug Fixes

--- api/routes/web.php
+++ api/routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
