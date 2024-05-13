eea8304db6d196b1ebe632f0351dafb32ddb1dfa
vasanthdommeti <139646696+vasanthdommeti@users.noreply.github.com>
2024-05-13 15:52:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 // import Login from './Components/LoginPage/Login';
-
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>
 );
+ 
