bfa1f10e7916b50c24a1710b5615d0c01d0df29b
pradeep_dgp <pradeepdgp0709@gmail.com>
2024-05-21 18:45:24 +0530

    minor changes-2

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -475,23 +475,15 @@ function Home() {
                                 </Link>
                             </div>
                         </div>
-                        {/* <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span className="visually-hidden">Previous</span>
                         </button>
                         <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                             <span className="carousel-control-next-icon" aria-hidden="true"></span>
                             <span className="visually-hidden">Next</span>
-                        </button> */}
+                        </button>
                     </div>
-                    {/* <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button> */}
-                    {/* <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button> */}
                 </div>
             </section>
             {/* //5 complete */}
