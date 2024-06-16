db1043dd353adb3e49cf10ca2b459c5a9d41cb00
NOOLU <noolu@apples-MacBook-Pro.local>
2024-06-17 00:16:06 +0530

    [FIX] navbar hover effect

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,9 +2,9 @@
 import React, { useEffect, useState } from 'react';
 import '../Navbar/Navbar.css'
 import logo from '../../Assets/LogoIcon.png'
-import Dropdown from 'react-bootstrap/Dropdown';
-import { MdClose } from "react-icons/md";
-import DropdownButton from 'react-bootstrap/DropdownButton';
+// import Dropdown from 'react-bootstrap/Dropdown';
+// import { MdClose } from "react-icons/md";
+// import DropdownButton from 'react-bootstrap/DropdownButton';
 import { IoClose, IoMenu } from "react-icons/io5";
 import { Link } from 'react-router-dom';
 function Navbar({ show }) {
@@ -19,116 +19,84 @@ function Navbar({ show }) {
     };
 
     return (
-        <nav style={{ display: show ? 'flex' : 'none' }}>
-            <div className='mainNavDiv'>
-                <div className="firstLogo">
-                    <Link to="/" className="title">
-                        <img src={logo} alt="logo" className='imagelogo' />
-                    </Link>
-                </div>
-                <div className="menu" onClick={closeNav}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
+        <nav style={{display:show ? 'flex' :'none'}}>
+            <div className="firstLogo">
+                <Link to="/section" className="title">
+                    <img src={logo} alt="logo" />
+                </Link>
             </div>
-            <div>
-                <div className='navmainDiv'>
-                <ul className={menuOpen ? "open" : "close"}>
-                    <li className='dropdown-li'>
-                        <Link to={'/home'} onClick={closeNav}>HOME</Link>
-                    </li>
-                    <li>
-                        <DropdownButton id="dropdown-basic-button" className="dropdown-about borderChange" title="ABOUT US">
-                            <Dropdown.Item as={Link} to="/aboutus/heritage" onClick={closeNav}>
-                                HERITAGE
-                            </Dropdown.Item>
-                            <Dropdown.Item as={Link} to="/aboutus/ourteam" onClick={closeNav}>
-                                OUR TEAM
-                            </Dropdown.Item>
-                            <Dropdown.Item as={Link} to="/aboutus/values" onClick={closeNav}>
-                                VALUES
-                            </Dropdown.Item>
-                            <Dropdown.Item as={Link} to="/aboutus/press" onClick={closeNav}>
-                                PRESS
-                            </Dropdown.Item>
-                        </DropdownButton>
-                    </li>
-                    <li>
-                        <DropdownButton id="dropdown-basic-button" drop='down' className="dropdown-about" title="PRODUCTS">
-                            <DropdownButton id="dropdown-basic-button" drop='left' className="dropdown-liquor dropdown-about" title="LIQUOR">
-                                <Dropdown.Item as={Link} to="/products/liquor/acpremiumold" onClick={closeNav}>
-                                    AC PREMIUM (OLD)
-                                </Dropdown.Item>
-                                <Dropdown.Item as={Link} to="/products/liquor/acpremium" onClick={closeNav}>
-                                    AC PREMIUM
-                                </Dropdown.Item>
-                                <Dropdown.Item as={Link} to="/products/liquor/acpremiumblack" onClick={closeNav}>
-                                    AC BLACK
-                                </Dropdown.Item>
-                                <Dropdown.Item as={Link} to="/products/liquor/royalpride" onClick={closeNav}>
-                                    ROYAL PRIDE
-                                </Dropdown.Item>
-                                <Dropdown.Item as={Link} to="/products/liquor/damngoodscotch" onClick={closeNav}>
-                                    DAMN GOOD SCOTCH
-                                </Dropdown.Item>
-                                <Dropdown.Item as={Link} to="/products/liquor/iicevodka" onClick={closeNav}>
-                                    IICE VODKA
-                                </Dropdown.Item>
-                                <Dropdown.Item as={Link} to="/products/liquor/acdrygin" onClick={closeNav}>
-                                    AC DRY GIN
-                                </Dropdown.Item>
-                                <Dropdown.Item as={Link} to="/products/liquor/royalmedallion" onClick={closeNav}>
-                                    ROYAL MEDALLION
-                                </Dropdown.Item>
-                            </DropdownButton> 
-                            {/* <Dropdown.Item as={Link} to="/products/mff" onClick={closeNav}>
-                                MFF
-                            </Dropdown.Item> */}
-                            <DropdownButton id="dropdown-basic-button" className="dropdown-liquor dropdown-about" title="MMF">
-                                <Dropdown.Item as={Link} to="/products/mmf/foodproducts" onClick={closeNav}>
-                                    FOOD PRODUCTS
-                                </Dropdown.Item>
-                                <Dropdown.Item as={Link} to="/products/mmf/maltextract" onClick={closeNav}>
-                                    MALT EXTRACT
-                                </Dropdown.Item>
-                            </DropdownButton>
-                            <Dropdown.Item as={Link} to="/products/ethnol" onClick={closeNav}>
-                                ETHNOL
-                            </Dropdown.Item>
-                        </DropdownButton>
-                    </li>
-                    <li className="secondLogo">
-                        <img src={logo} alt="logo" />
-                    </li>
-                    <li className='dropdown-li'>
-                        <Link to="/ourprocess" onClick={closeNav}>
+            <div className="menu" onClick={closeNav}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className='headerNavBar'>
+                <div className={menuOpen ? "open" : "mainNavDiv"}>
+                    <div class="navItem">
+                    <Link to={'/home'} onClick={closeNav} className='linkText'>HOME</Link>
+                    </div>
+                    <div class="navItem">
+                        <h4>ABOUT US</h4>
+                        <div class="subItems">
+                            <Link to="/aboutus/heritage">HERITAGE</Link>
+                            <Link to="/aboutus/ourteam">OUR TEAM</Link>
+                            <Link to='/aboutUs/values'>VALUES</Link>
+                            <Link to="/aboutus/press">PRESS</Link>
+                        </div>
+                    </div>
+                    <div className="navItem1">
+                        <h4>PRODUCTS</h4>
+                        <div className="subItems1">
+                            <div className="menu-link2"><h5>LIQUOR</h5>
+                                <div className="liquorSubItems1">
+                                    <Link to="/products/liquor/acpremiumold" >AC PREMIUM (OLD)</Link>
+                                    <Link to="/products/liquor/acpremium">AC PREMIUM (NEW)</Link>
+                                    <Link to="/products/liquor/acpremiumblack">AC BLACK</Link>
+                                    <Link to="/products/liquor/royalpride">ROYAL PRIDE</Link>
+                                    <Link to="/products/liquor/damngoodscotch">DAMN GOOD SCOTCH</Link>
+                                    <Link to="/products/liquor/iicevodka">IICE VODKA</Link>
+                                    <Link to="/products/liquor/acdrygin">AC DRY GIN</Link>
+                                    <Link to="/products/liquor/royalmedallion">ROYAL MEDALLION</Link>
+                                </div>
+                            </div>
+                            <div className='navItem2'>
+                                <div className="subItem2">
+                                    <div className="menu-link"><h5>MFF</h5>
+                                        <div className="liquorSubItems2">
+                                            <Link to="/products/mmf/foodproducts" >FOOD PRODUCTS</Link>
+                                            <Link to='/products/liquor/ACpremiumNew'>MALT EXTRACT</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                                <Link to="/products/ethnol" >ETHNOL</Link>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="secondLogo">
+                        <img src={logo} alt='logo' />
+                    </div>
+                    <div class="navItem">
+                    <Link to="/ourprocess" className='linkText' onClick={closeNav}>
                             OUR PROCESS
                         </Link>
-                    </li>
-                    <li>
-                        <DropdownButton id="dropdown-basic-button" className="dropdown-about" title="INVESTORS">
-                            <Dropdown.Item as={Link} to="/investors/financial" onClick={closeNav}>
-                                FINANCIAL
-                            </Dropdown.Item>
-                            <Dropdown.Item as={Link} to="/investors/reports" onClick={closeNav}>
-                                REPORTS
-                            </Dropdown.Item>
-                            <Dropdown.Item as={Link} to="/investors/csr" onClick={closeNav}>
-                                CSR
-                            </Dropdown.Item>
-                            <Dropdown.Item as={Link} to="/investors/codeofconduct" onClick={closeNav}>
-                                CODE OF CONDUCT
-                            </Dropdown.Item>
-                        </DropdownButton>
-                    </li>
-                    <li className='dropdown-li'>
-                        <Link to="/contactus" onClick={closeNav}>CONTACT US</Link>
-                    </li>
-                </ul>
+                    </div>
+                    <div class="navItem">
+                        <h4>INVESTORS</h4>
+                        <div class="subItems">
+                            <Link to="/investors/financial">FINANCIAL</Link>
+                            <Link to="/investors/reports">REPORTS</Link>
+                            <Link to="/investors/csr" >CSR</Link>
+                            <Link to="/investors/codeofconduct">CODE OF CUNDUCT</Link>
+                        </div>
+                    </div>
+                    <div class="navItem">
+                    <Link to="/contactus" className='linkText' onClick={closeNav}>CONTACT US</Link>
+                    </div>
                 </div>
-                <div className="menu" style={{ zIndex: 2, marginRight: '7%' }} id="nav-close" onClick={closeNav}>
-                    {menuOpen && <IoClose fontSize={75} />}
+                <div className="menu" style={{zIndex:2, marginRight:'7%'}} id="nav-close" onClick={closeNav}>
+                    {menuOpen &&<IoClose fontSize={75}/>}
                 </div>
             </div>
         </nav>
